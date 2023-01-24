163148477a0d1a481bbb47008ce9dba89c6d2240
Shaun Hutchinson <shutchnz@gmail.com>
2023-01-24 15:57:20 +1300

    Try to get lambda deployed through Amplify build

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,3 +1,7 @@
+echo checking OS...
+if [[ $OSTYPE != *'darwin'* ]]; then
+    apt-get install -y dotnet-sdk-7.0
+fi
 echo bundling lambda...
 dotnet lambda deploy-function -fn dinner-ideas -pl api/src/dinner-ideas
 echo lambda deployed!
