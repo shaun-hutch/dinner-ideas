8c954a75145c6daeebe7c4b48eb26dfade0cc433
Shaun <shutchnz@gmail.com>
2023-04-10 22:09:57 +1200

    Got frontend going with backend.

--- app/dinner-ideas/src/index.tsx
+++ app/dinner-ideas/src/index.tsx
@@ -23,6 +23,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
