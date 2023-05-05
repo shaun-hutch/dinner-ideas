1a7fa5355969bd5f281d3564582e3024370ed013
Shaun Hutchinson <shutchnz@gmail.com>
2023-05-05 22:37:40 +1200

    Removed not working auth.

--- app/dinner-ideas/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/dinner-ideas/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,16 +5,5 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
-  },
-  "auth": {
-    "dinneridease6175b8e": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
   }
 }
