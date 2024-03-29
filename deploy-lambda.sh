a418d06886382a8d61028ee716e9e0f85de9e9b3
Shaun Hutchinson <shutchnz@gmail.com>
2024-03-29 19:31:50 +1300

    Removed amplify app and put in default create-react-app

--- deploy-lambda.sh
+++
@@ -1,7 +0,0 @@
-echo checking OS...
-if [[ $OSTYPE != *'darwin'* ]]; then
-    apt-get install -y dotnet-sdk-7.0
-fi
-echo bundling lambda...
-dotnet lambda deploy-function -fn dinner-ideas -pl api/src/dinner-ideas
-echo lambda deployed!
