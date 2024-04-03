8c118293cf97200d424a618777be77270e1a33c0
Shaun Hutchinson <shutchnz@gmail.com>
2024-04-03 17:46:16 +1300

    Add basic router and UI component library

--- dinner-ideas/src/index.tsx
+++ dinner-ideas/src/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { PrimeReactProvider } from 'primereact/api';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: App()
+    }
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <PrimeReactProvider>
+            <RouterProvider router={router} />
+        </PrimeReactProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
