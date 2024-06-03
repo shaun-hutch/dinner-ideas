8003adbc3d876ab858d1b60ab14cd739ecd6906d
Shaun <shutchnz@gmail.com>
2024-06-03 23:27:17 +1200

    Uncomment update lambda shell commands.

--- dinner-ideas-api/update-lambda.sh
+++ dinner-ideas-api/update-lambda.sh
@@ -1,5 +1,3 @@
-# aws lambda update-function-code \
-# --function-name dinner-ideas-lambda-api \
-# --zip-file fileb://function.zip \
-
-
+aws lambda update-function-code \
+--function-name dinner-ideas-lambda-api \
+--zip-file fileb://function.zip \
