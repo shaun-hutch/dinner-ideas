51874b418e15b7c9cbb1d789eabd7743a14d56ce
Shaun <shutchnz@gmail.com>
2024-07-18 22:42:07 +1200

    Remove unused imports

--- dinner-ideas/src/App.tsx
+++ dinner-ideas/src/App.tsx
@@ -1,8 +1,4 @@
 import './App.css';
-import { DinnerItem } from './models/DinnerItem';
-import { FoodTag } from './models/FoodTag';
-import { ApiEndpoint } from './models/Constants';
-import DinnerListItem from './components/DinnerListItem/DinnerListItem';
 import DinnerList from './components/DinnerList/DInnerList';
 import { componentKey } from './helpers/componentHelpers';
 
