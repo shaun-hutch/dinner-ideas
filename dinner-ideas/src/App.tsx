a3a5b0c59f06aec362ba17e72235366f97db5457
Shaun <shutchnz@gmail.com>
2024-07-20 20:54:41 +1200

    See if build works without component reference

--- dinner-ideas/src/App.tsx
+++ dinner-ideas/src/App.tsx
@@ -1,10 +1,8 @@
-import DinnerList from 'components/DinnerList/DinnerList';
 import './App.css';
 
 const App = () => {
     return (
         <div className="body">
-            <DinnerList />
         </div>
     );
 };
