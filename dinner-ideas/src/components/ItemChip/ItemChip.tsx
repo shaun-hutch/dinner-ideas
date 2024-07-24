d53bdc3657d7dab46f030d9e678f9d090404461e
Shaun <shutchnz@gmail.com>
2024-07-24 23:50:25 +1200

    Remove unused imports again

--- dinner-ideas/src/components/ItemChip/ItemChip.tsx
+++ dinner-ideas/src/components/ItemChip/ItemChip.tsx
@@ -1,8 +1,6 @@
-import styled from "styled-components";
 import { FoodTag } from "../../models/FoodTag"
 import './ItemChip.css';
 import { FoodTagColor, FoodTagLabel } from "../../models/Constants";
-import { componentKey } from "../../helpers/componentHelpers";
 import { Chip } from "primereact/chip";
 
 interface ItemChipProps {
@@ -20,15 +18,5 @@ const ItemChip = (props: ItemChipProps) => {
         <Chip label={FoodTagLabel[FoodTag]} style={{background: FoodTagColor[FoodTag]}} className="item-chip" />
     )
 };
-
-// const Chip = styled.div<{ $tag?: FoodTag; }>`
-//         background-color: ${props => FoodTagColor[props.$tag as FoodTag]};
-//         border-radius: 1em;
-//         padding-left: 1em;
-//         padding-right: 1em;
-//         margin-right: 0.5em;
-//         margin-top: 0.5em;
-//         font-size: 0.8em;
-//     `;
 
 export default ItemChip;
