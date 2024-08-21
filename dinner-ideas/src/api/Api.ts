fb2d45548ddaa076f0ed11c50eb83ed719e48caf
Shaun <shutchnz@gmail.com>
2024-08-21 22:38:28 +1200

    Begin work on updating an item.

--- dinner-ideas/src/api/Api.ts
+++ dinner-ideas/src/api/Api.ts
@@ -1,11 +1,11 @@
 import { ApiEndpoint } from "../models/Constants";
 import { DinnerItem } from "../models/DinnerItem";
 
-const baseEndpoont = `${ApiEndpoint}/dinner-ideas-db`;
+const baseEndpoint = `${ApiEndpoint}/dinner-ideas-db`;
 
 export const getAll = async (): Promise<DinnerItem[]> => {
     try {
-        const response = await fetch(baseEndpoont);
+        const response = await fetch(baseEndpoint);
         if (!response.ok) {
             throw new Error(`Error: ${response.status}, ${response}`);
         }
@@ -18,3 +18,23 @@ export const getAll = async (): Promise<DinnerItem[]> => {
         throw error;
     }
 };
+
+export const update = async (item: DinnerItem): Promise<DinnerItem> => {
+    try {
+        const response = await fetch(baseEndpoint, {
+            method: "PUT",
+            body: JSON.stringify(item)
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}, ${response}`);
+        }
+
+        const data: DinnerItem = await response.json();
+
+        return data;
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
--- dinner-ideas/src/components/DinnerItemEditor/DinnerItemEditor.tsx
+++ dinner-ideas/src/components/DinnerItemEditor/DinnerItemEditor.tsx
@@ -1,26 +1,32 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './DinnerItemEditor.css';
-import { DinnerItemContext, useDiinnerItemListContext } from 'hooks/useDinnerItemListContext';
-import { useContext, useEffect, useState } from 'react';
-import { classNames } from 'primereact/utils';
+import { DinnerItemContext } from 'hooks/useDinnerItemListContext';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { InputText } from 'primereact/inputtext';
-import { DinnerItem } from 'models/DinnerItem';
 import './DinnerItemEditor.css';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { FloatLabel } from "primereact/floatlabel";
+import { Button } from 'primereact/button';
+import { update } from 'api/Api';
+import { DinnerItem } from 'models/DinnerItem';
 
 interface DinnerItemEditorProps {
     readOnly?: boolean;
 }
 
-
 const DinnerItemEditor = (props: DinnerItemEditorProps) => {
+    const navigate = useNavigate();
+
     const {
         readOnly
     } = props;
 
-    const [dinnerItem, setDinnerItem] = useState<DinnerItem>();
     const [loaded, setLoaded] = useState<boolean>(false);
+    const [isSaving, setIsSaving] = useState<boolean>(false);
+    const [dinnerItem, setDinnerItem] = useState<DinnerItem>();
+
+    const [name, setName] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
 
     const { dinnerItemId } = useParams();
     const { getDinnerItem } = useContext(DinnerItemContext);
@@ -28,33 +34,69 @@ const DinnerItemEditor = (props: DinnerItemEditorProps) => {
     useEffect(() => {
         if (dinnerItemId && getDinnerItem) {
             const item = getDinnerItem(dinnerItemId);
-            if (item && setDinnerItem) {
+            if (item && !loaded) {
+                setLoaded(true);
                 setDinnerItem(item);
+
+                // set all the other items
+                setName(item.name);
+                setDescription(item.description);
             }
         }
+    }, [dinnerItemId, getDinnerItem, loaded]);
 
-    }, [dinnerItemId, getDinnerItem, setDinnerItem]);
+    const onNameChange = useCallback((content: string) => {
+        console.log('name change', content);
+        setName(content);
+    },[setName]);
 
-    useEffect(() => {
-        if (dinnerItem && !loaded) {
-            setLoaded(true);
-        }
-    }, [dinnerItem, loaded, setLoaded]);
+    const onDescriptionChange = useCallback((content: string) => {
+        setDescription(content);
+    },[setName]);
+
+    const onSave = useCallback(() => {
+        const payload: DinnerItem = {
+            ...dinnerItem!,
+            name,
+            description
+        };
+        setIsSaving(true);
+
+            update(payload).then((response: DinnerItem) => {
+                setIsSaving(false);
+                console.log(response);
+                navigate('/');
+            }, (error) => {
+                setIsSaving(false);
+                console.error(error);
+            });
+    }, [navigate, dinnerItem, setIsSaving, name, description]);
 
     return (
         <div className="dinner-item-form">
             {readOnly ? (
                 <div className="dinner-item-form-viewer">
-                    <h2>{dinnerItem?.name}</h2>
-                    <p>{dinnerItem?.description}</p>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
                 </div>
             ) : (
                 <div className="dinner-item-form-editor">
-                    <FloatLabel>
-                        <InputText id="name" className="dinner-item-text-input" value={dinnerItem?.name} />
-                        <label htmlFor="name">Name</label>
-                    </FloatLabel>
-                    <InputTextarea className="dinner-item-text-input" value={dinnerItem?.description} />
+                    <div className="dinner-item-form-field">
+                        <FloatLabel>
+                            <InputText id="name" className="dinner-item-text-input" value={name} onChange={e => onNameChange(e.target.value)} />
+                            <label htmlFor="name">Name</label>
+                        </FloatLabel>
+                    </div>
+                    <div className="dinner-item-form-field">
+                        <FloatLabel>
+                            <InputTextarea id="description" className="dinner-item-text-input" value={description} onChange={e => onDescriptionChange(e.target.value)}/>
+                            <label htmlFor="description">Description</label>
+                        </FloatLabel>
+                    </div>
+
+                    <div className="dinner-item-form-buttons">
+                        <Button icon="pi pi-save" className="save-button" raised rounded onClick={onSave} label="Save" disabled={isSaving} />
+                    </div>
                 </div>
             )}
 
