6715d0e73bb5d69073a96f0c26e82a348e51bd2b
Shaun <shutchnz@gmail.com>
2025-01-29 22:29:15 +1300

    Work on setting up model, sample data and basic list

---
+++ dinner-ideas-ios/dinner-ideas/dinner-ideas/dinner_ideasApp.swift
@@ -0,0 +1,17 @@
+//
+//  dinner_ideasApp.swift
+//  dinner-ideas
+//
+//  Created by Shaun Hutchinson on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct dinner_ideasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
